4996d7d0383854aff511b47b4e84631a75eacebd
misssswa1123 <missswapnali23@gmail.com>
2026-09-19 00:57:55 +0530

    Added http method get'

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
-
+import json
 app=FastAPI()
+
+def data_load():
+    with open('pat.json','r') as f:
+        data=json.load(f)
+    return data
+
+@app.get('/view')
+def view():
+    data=data_load()
+    return data
 
 @app.get('/')
 def hello():
